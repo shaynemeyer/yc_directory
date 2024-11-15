6fae32c9984fb826feab595aa728393c78ad3837
Shayne Meyer <shayne.meyer@gmail.com>
2024-11-14 21:14:15 -0800

    added startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,19 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+
+const posts = [
+  {
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: "John Smalls" },
+    _id: 1,
+    description: "This is a description",
+    image:
+      "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.03&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Robots",
+    title: "We Robots",
+  },
+];
 
 export default async function Home({
   searchParams,
@@ -20,6 +35,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );

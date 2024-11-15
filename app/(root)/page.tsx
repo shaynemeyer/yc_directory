5cb53ca8016d5c375c97a029dd3b8f01eadd6106
Shayne Meyer <shayne.meyer@gmail.com>
2024-11-14 20:28:27 -0800

    added search component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,13 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  // const params = { search: query || null };
+
   return (
     <>
       <section className="pink_container">
@@ -9,6 +18,8 @@ export default function Home() {
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
